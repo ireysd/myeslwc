bbce8b6ea6ed73a57318b3a9eb00771f73f2ee6b
Durana <irey.shaira.a.durana@accenture.com>
2019-11-05 10:06:13 +0800

    init

---
+++ force-app/main/default/lwc/reservationHelper/reservationHelper.js
@@ -0,0 +1,62 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { CurrentPageReference } from 'lightning/navigation';
+import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
+
+export default class ReservationHelper extends LightningElement 
+{
+    @track flowStarted = false;
+    @wire(CurrentPageReference) pageRef;
+
+
+    connectedCallback() 
+    {
+        registerListener('selectcustomer', this.handleCustomerSelect, this);
+    }
+
+
+    disconnectedCallback() 
+    {
+        unregisterAllListeners(this);
+    }
+
+
+    handleCustomerSelect(event) 
+    {
+        if (!this.flowStarted) 
+        {
+            // eslint-disable-next-line no-debugger
+            debugger;
+            // eslint-disable-next-line no-console
+            console.log('here!  3');
+
+            this.flowStarted = true;
+            this.dispatchEvent(
+                new CustomEvent('customerchoice', { detail: event.detail }),
+            );
+        } 
+        else if (this.flowStarted) 
+        {
+            // eslint-disable-next-line no-debugger
+            debugger;
+            // eslint-disable-next-line no-console
+            console.log('here! 3a');
+       
+            const toastEvt = new ShowToastEvent({
+                title: 'Flow interview already in progress',
+                message:
+                    'Finish the flow interview in progress before selecting another customer.',
+                variant: 'error',
+            });
+
+            this.dispatchEvent(toastEvt);
+        }
+    }
+
+
+    @api
+    handleFlowExit(event) 
+    {
+        fireEvent(this.pageRef, 'flowfinish', event);
+    }
+}
